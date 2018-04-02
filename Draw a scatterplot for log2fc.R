9ee97f5c6df1537b08163a7bf986cb67db1c30ef
Wan-Yifei <wanyifei0123@126.com>
2018-04-02 16:06:14 -0500

    feat: Push this project
    This is a R project which focuses on RNAseq research. Some relative code would be created and pushed.
    
    doc:
    -1.READ.ME
    -2. 'Draw a scatterplot for log2fc.r' V 1.0

---
+++ Draw a scatterplot for log2fc.R
@@ -0,0 +1,24 @@
+# draw a scatter plot for log2 fold change
+library("ggplot2")
+
+## load raw data
+rawdata <- read.csv2('LncRNA_all.txt', sep = '\t', row.names = 1)
+
+## calculate the fold change
+data1 <- log2(rawdata + 1)
+log2fc <- data1$Mod.1_count - data1$Treatment.1_count
+
+## evalute the  Up regulated or down regulated based on threshold
+judge <- sapply(log2fc, function(x , threshold = 1){ ## default threshold =1
+  if(x > threshold){x = 'up'
+  } else if (x < -threshold){x = 'down'
+  } else {x = 'none'}
+})
+
+## draw the scatter plot
+ggplot(data = data1, aes(x=Mod.1_count, y=Treatment.1_count) ) + 
+  geom_point(aes(color=judge), alpha=1/2, size=0.8) + ## decide the color relying on 'judge'
+  scale_color_manual(values=c("firebrick1", "grey", "forestgreen")) + 
+  geom_smooth(method="lm",se=FALSE) ## add trend line
+
+
