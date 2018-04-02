534629924cea71442a05d236adac5eab29aa00a9
Wan-Yifei <wanyifei0123@126.com>
2018-04-02 16:21:01 -0500

    fix: exchange x with y exchange varibales to show the proper colors. #19

--- Draw a scatterplot for log2fc.R
+++ Draw a scatterplot for log2fc.R
@@ -16,7 +16,7 @@ judge <- sapply(log2fc, function(x , threshold = 1){ ## default threshold =1
 })
 
 ## draw the scatter plot
-ggplot(data = data1, aes(x=Mod.1_count, y=Treatment.1_count) ) + 
+ggplot(data = data1, aes(x=Treatment.1_count, y=Mod.1_count) ) + 
   geom_point(aes(color=judge), alpha=1/2, size=0.8) + ## decide the color relying on 'judge'
   scale_color_manual(values=c("firebrick1", "grey", "forestgreen")) + 
   geom_smooth(method="lm",se=FALSE) +  ## add trend line
